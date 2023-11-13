a4b93736d3a8bb506f7e7c34559befb905774af3
hawk <wagner-harry@web.de>
2023-11-13 01:43:54 +0100

    it seems to work

--- year2022/day9/part2/cpp/main.cpp
+++ year2022/day9/part2/cpp/main.cpp
@@ -77,14 +77,45 @@ struct coordinate_t {
         //*this == coord
         return false;
     }
+
+    bool operator==(const coordinate_t& left) const {
+        return (x == left.x && y == left.y);
+    }
 };
 
-struct knot_t {
+struct rope_item {
     coordinate_t _position;
     direction_t _direction;
+
+    virtual void make_a_step(const rope_item& predecessor) {
+        const auto diff_x = predecessor._position.x - _position.x;
+        const auto diff_y = predecessor._position.y - _position.y;
+        if((abs(diff_x) > 1) || abs(diff_y) > 1) {
+            if(diff_x != 0) {
+                _position.x = _position.x + ((diff_x < 0) ? -1 : 1);
+            }
+            if(diff_y != 0 ) {
+                _position.y = _position.y + ((diff_y < 0) ? -1 : 1);
+            }
+        }
+//        if((abs(diff_y) > 1)) {
+//            _position.y = predecessor._position.y + -1 * predecessor._direction.getY();
+//            _position.x = _position.x + _direction.getX();
+//        }
+//        _direction.setX( predecessor._position.x - _position.x);
+//        _direction.setY( predecessor._position.y - _position.y);
+    }
+
+    void print_debug() const
+    {
+        std::cout << "Direction: " << _direction.getX() << " : " << _direction.getY() << std::endl;
+        std::cout << "Position: " << _position.x << " : " << _position.y << std::endl;
+
+    }
+
 };
 
-struct head_t : public knot_t {
+struct head_t : public rope_item {
     void make_a_step(const direction_t& direction) {
         _direction = direction;
         _position.x += _direction.getX();
@@ -92,58 +123,79 @@ struct head_t : public knot_t {
     }
 };
 
-struct tail_t : public knot_t {
+struct tail_t : public rope_item {
+    std::vector<coordinate_t> _positions;
 
-    std::set<coordinate_t> _positions;
+    void make_a_step(const rope_item& predecessor) override{
+        rope_item::make_a_step(predecessor);
 
-    tail_t() {
-        _positions.insert(_position);
-    }
-
-    void make_a_step(const knot_t& knot) {
-        const auto diff_x = abs(knot._position.x - _position.x);
-        const auto diff_y = abs(knot._position.y - _position.y);
-        if((diff_x > 1) || diff_y > 1 ) {
-            direction_t move_dir;
-            move_dir.setX(  knot._direction.getX() == 0 ? _direction.getX() : knot._direction.getX() );
-            move_dir.setY(  knot._direction.getY() == 0 ? _direction.getY() : knot._direction.getY() );
-
-            _position.x = _position.x + 1 * move_dir.getX();
-            _position.y = _position.y + 1 * move_dir.getY();
-            _direction = knot._direction;
+        bool found = false;
+        for( const auto& pos : _positions) {
+            if(pos == _position) {
+                found = true;
+                break;
+            }
         }
-
-        if(!_positions.contains(_position)) {
-            _positions.insert(_position);
+        if( ! found ) {
+            _positions.push_back(_position);
         }
     }
+
 };
 
 struct rope_t {
 
     head_t head;
-    std::array<tail_t, 1> tails;
+    tail_t tail;
+    constexpr static int max_knots = 8;
+    std::array<rope_item, max_knots> knots;
 
     void move(const std::string& move_command) {
-
+        std::cout << "Command: " << move_command << "\n";
         auto new_direction = direction_t::create_direction(move_command);
         auto is_direction_changed = (new_direction != head._direction);
 
         auto steps = static_cast<int>(std::strtol( move_command.substr(2).c_str(), nullptr, 10 ));
         for(int i = 0; i < steps; i++) {
+            std::cout << "Step: " << i << std::endl;
             head.make_a_step(new_direction);
-            tails[0].make_a_step(head);
-            for( int t = 1; t < tails.size(); t++) {
-                tails[t].make_a_step(tails[t-1]);
+
+            std::cout << " head: \n";
+            head.print_debug();
+
+            knots[0].make_a_step(head);
+            std::cout << " node 0: \n";
+            knots[0].print_debug();
+            for(int n=1; n < max_knots; n++) {
+                knots[n].make_a_step(knots[n-1]);
+                std::cout << " node " << n << ": \n";
+                knots[n].print_debug();
             }
+            tail.make_a_step(*knots.rbegin());
+            std::cout << " tail: \n";
+            tail.print_debug();
         }
 
     }
+
+    [[nodiscard]] std::size_t tail_positions_count()const {
+        return tail._positions.size();
+        }
+
+    void tail_positions()const {
+        std::cout << "--\n";
+        for(const auto& pos:  tail._positions) {
+            std::cout << "x: " << pos.x << " | y: " << pos.y << "\n";
+        }
+    }
+
+    void print_debug()const {
+        for(const auto& ri : knots) {
+            ri.print_debug();
+        }
+    }
 };
-
-
 int main() {
-
 
     const std::string input_file = "./input_data.txt";
     std::ifstream input_data(input_file);
@@ -160,7 +212,11 @@ int main() {
     input_data.close();
 
 
-    std::cout << "Solution: " << rope.tails[rope.tails.size()-1]._positions.size() << std::endl;
+    std::cout << "Solution: " << rope.tail_positions_count() << std::endl;
+
+    rope.print_debug();
+    rope.tail_positions();
+
     return 0;
 }
 
