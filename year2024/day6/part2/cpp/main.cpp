f63a31a35a71d89f9892e64d12e6aa6ac07f489b
Harry Wagner <wagner-harry@web.de>
2025-10-17 16:51:25 +0200

    day6_part2

--- year2024/day6/part2/cpp/main.cpp
+++ year2024/day6/part2/cpp/main.cpp
@@ -1,32 +1,15 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <trim_functions.h>
-#include <regex>
-#include <set>
-#include <sstream>
+//import std;
+import <iostream>;
+// #include <fstream>
+// #include <string>
+//#include <trim_functions.h>
+//#include <regex>
+//#include <set>
+//#include <sstream>
 
 int main() {
 
     const std::string input_file = "./input_data.txt";
-    std::ifstream input_data(input_file);
-    if(!input_data.is_open()) {
-        std::cerr << "cannot find file " << input_file << std::endl;
-        return 1;
-    }
-    long sum  = 0;
-
-    std::string text_line;
-
-    bool readRules = true;
-    while(std::getline(input_data, text_line))
-    {
-        string_trimmer::trim(text_line);
-        if (text_line.empty())
-        {
-            break;
-        }
-    }
-    std::cout << "Solution: " << sum << std::endl;
+    std::cout << "Solution: " << input_file << std::endl;
     return 0;
 }
