5da04b3afd611da8612a772bbfdf913f1198bf55
Harry Wagner <wagner-harry@web.de>
2025-11-16 22:20:32 +0100

    day6 part1 works same as part2

--- year2024/day6/part1/cpp/main.cpp
+++ year2024/day6/part1/cpp/main.cpp
@@ -1,90 +1,59 @@
+#include <format>
 #include <fstream>
 #include <iostream>
+#include <optional>
 #include <set>
 #include <string>
-#include <trim_functions.h>
 #include <utility>
-#include <vector>
+
+#include "room.h"
 
 using position_t = std::pair<int, int>;
 using direction_t = std::pair<int, int>;
-
-bool do_next_step(const std::vector<std::string>& room, const std::set<position_t>& obstructions, position_t& guard_position, direction_t& guard_direction) {
-  int next_x = guard_position.first + guard_direction.first;
-  int next_y = guard_position.second + guard_direction.second;
-  if (next_x >= room[0].size() || next_y >= room.size() || next_x < 0 || next_y < 0)
-  {
-    //out of room border
-    return false;
-  }
-  position_t next_guard_position = {next_x, next_y};
-
-  if (obstructions.contains(next_guard_position))
-  {
-    //turn the guard in the right direction
-    int x =  guard_direction.second * -1;
-    int y = guard_direction.first * 1;
-    guard_direction = {x, y};
-
-    return do_next_step(room, obstructions, guard_position, guard_direction);
-  }
-  else
-  {
-    guard_position = next_guard_position;
-  }
-  return true;
-}
-
+/*
+ *How to find loops
+  When the guard visits a new position, it must be checked by placing an
+ imaginary obstacle whether he will ever return to that position.
+*/
 
 int main() {
-
   const std::string input_file = "./input_data.txt";
   std::ifstream input_data(input_file);
   if (!input_data.is_open()) {
-    std::cerr << "cannot find file " << input_file << std::endl;
+    std::cerr << std::format("Cannot find file: '{0}'.", input_file);
     return 1;
   }
-  long sum = 0;
 
   std::string text_line;
 
-  std::vector<std::string> room;
-  std::set<position_t> obstructions;
-  position_t guard_position;
-  direction_t guard_direction{0,-1};  //up
-
-  while (std::getline(input_data, text_line)) {
-    string_trimmer::trim(text_line);
-    if (text_line.empty()) {
-      break;
-    }
-    room.push_back(text_line);
-
-    for (int x = 0; x < text_line.size(); ++x) {
-      if (text_line[x] == '#') {
-        auto insert_result = obstructions.insert(std::make_pair(x, room.size()-1));
-        if (!insert_result.second) {
-          std::cerr << "cannot insert " << x << std::endl;
-          return 1;
-        }
-      }
-      else if (text_line[x] == '^') {
-        guard_position.first = x;
-        guard_position.second = static_cast<int>(room.size()-1);
-      }
-    }
-  }
-
+  auto room = Room::Create(input_data);
   input_data.close();
 
-  std::set<position_t> guard_positions;
-  guard_positions.insert(guard_position);
-
-  while( do_next_step(room, obstructions, guard_position, guard_direction) )
-  {
-    guard_positions.insert(guard_position);
+  if (room == nullptr) {
+    std::cerr << std::format("Cannot read data in file '{0}'.\n", input_file);
+    return 1;
   }
 
-  std::cout << "Solution: " << guard_positions.size() << std::endl;
+  std::set<Position> guard_positions;
+  guard_positions.insert(room->GetGuard().GetPosition());
+
+  // here we check if is possible to place an obstacle in front of guard
+  auto next_possible_guard_position =
+      room->GetNextPossibleGuardPosition(room->GetGuard());
+
+  while (next_possible_guard_position.has_value()) {
+
+    std::cout << std::format(
+        "Next possible guard_position : {0} {1}\n",
+        next_possible_guard_position.value().GetPosition().X(),
+        next_possible_guard_position.value().GetPosition().Y());
+
+    guard_positions.insert(next_possible_guard_position.value().GetPosition());
+    room->PlaceGuard(next_possible_guard_position.value());
+    next_possible_guard_position = room->GetNextPossibleGuardPosition(
+        next_possible_guard_position.value());
+  }
+
+  std::cout << std::format("Solution: {0}\n", guard_positions.size());
   return 0;
 }
