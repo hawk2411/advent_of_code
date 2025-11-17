3615c8697d526f44de939e7d05eafb0f1c182057
Harry Wagner <wagner-harry@web.de>
2025-11-17 14:02:52 +0100

    ---

--- year2024/day6/part2/cpp/main.cpp
+++ year2024/day6/part2/cpp/main.cpp
@@ -37,39 +37,35 @@ int main() {
   std::set<Position> obstructions;
 
   // here we check if is possible to place an obstacle in front of guard
-  auto next_possible_guard_position =
-      room->GetNextPossibleGuardPosition(room->GetGuard());
+  auto next_position = room->GetNextPossibleGuardPosition(room->GetGuard());
 
-  while (next_possible_guard_position.has_value()) {
-    std::cout << std::format(
-        "Obstacle position: {0} {1}\n",
-        next_possible_guard_position.value().GetPosition().X(),
-        next_possible_guard_position.value().GetPosition().Y());
+  while (next_position.has_value()) {
+
+    std::cout << std::format("Obstacle position: {0} {1}\n",
+                             next_position.value().GetPosition().X(),
+                             next_position.value().GetPosition().Y());
+
     Room try_room = *room;
     // place the obstacle and let the guard runs again it
-    if (!try_room.PlaceObstruction(
-            next_possible_guard_position.value().GetPosition())) {
-      std::cerr << "cannot place obstruction\n";
+    if (!try_room.PlaceObstruction(next_position.value().GetPosition())) {
+      std::cout << "cannot place obstruction\n";
       return 1;
     }
     // now we let guard continue his patrol. If he goes out of bounds then we
     // have no loop but if he comes back to guard_position then we have a loop
     if (try_room.IsGuardInLoop()) {
-      auto insert_result =
-          obstructions.insert(next_possible_guard_position->GetPosition());
+      auto insert_result = obstructions.insert(next_position->GetPosition());
       if (!insert_result.second) {
-        std::cout << std::format(
-            "cannot insert obstruction: X: {0} {1}\n",
-            next_possible_guard_position->GetPosition().X(),
-            next_possible_guard_position->GetPosition().Y());
+        std::cout << std::format("cannot insert obstruction: X: {0} {1}\n",
+                                 next_position->GetPosition().X(),
+                                 next_position->GetPosition().Y());
       } else {
         std::cout << std::format("Found a loop. Current count: {0}\n",
                                  obstructions.size());
       }
     }
-    room->PlaceGuard(next_possible_guard_position.value());
-    next_possible_guard_position = room->GetNextPossibleGuardPosition(
-        next_possible_guard_position.value());
+    room->PlaceGuard(next_position.value());
+    next_position = room->GetNextPossibleGuardPosition(next_position.value());
   }
 
   std::cout << std::format("Solution: {0}\n", obstructions.size());
