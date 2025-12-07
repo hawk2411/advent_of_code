db805b8dcfeaef8962d040dc099c5653b4613ae2
Harry Wagner <wagner-harry@web.de>
2025-12-07 01:37:12 +0100

    solved 2024 day 7 part1

--- global_inc/trim_functions.h
+++ global_inc/trim_functions.h
@@ -6,51 +6,72 @@
 #define ADVENT_OF_CODE_TRIM_FUNCTIONS_H
 #include <algorithm>
 #include <string>
+#include <vector>
 
 struct string_trimmer {
-    static inline void ltrim(std::string &s) {
-        s.erase(s.begin(), std::ranges::find_if(s, [](unsigned char ch) {
-            return !std::isspace(ch);
-        }));
-    }
+  static inline void ltrim(std::string &s) {
+    s.erase(s.begin(), std::ranges::find_if(s, [](unsigned char ch) {
+              return !std::isspace(ch);
+            }));
+  }
 
-// trim from end (in place)
-    static inline void rtrim(std::string &s) {
-        s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
-            return !std::isspace(ch);
-        }).base(), s.end());
-    }
+  // trim from end (in place)
+  static inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(),
+                         [](unsigned char ch) { return !std::isspace(ch); })
+                .base(),
+            s.end());
+  }
 
-// trim from both ends (in place)
-    static inline void trim(std::string &s) {
-        ltrim(s);
-        rtrim(s);
-    }
-
+  // trim from both ends (in place)
+  static inline void trim(std::string &s) {
+    ltrim(s);
+    rtrim(s);
+  }
 };
 
-namespace string_functions
-{
-    inline std::string ltrim(std::string s) {
-        s.erase(s.begin(), std::ranges::find_if(s, [](unsigned char ch) {
+namespace string_functions {
+inline std::string ltrim(std::string s) {
+  s.erase(s.begin(), std::ranges::find_if(s, [](unsigned char ch) {
             return !std::isspace(ch);
-        }));
-        return s;
-    }
-
-    inline std::string rtrim(std::string s) {
-        s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
-            return !std::isspace(ch);
-        }).base(), s.end());
-
-        return s;
-    }
-
-    inline std::string trim(std::string s){
-        return rtrim(ltrim(s));
-    }
+          }));
+  return s;
 }
+
+inline std::string rtrim(std::string s) {
+  s.erase(std::find_if(s.rbegin(), s.rend(),
+                       [](unsigned char ch) { return !std::isspace(ch); })
+              .base(),
+          s.end());
+
+  return s;
+}
+
+inline std::string trim(std::string s) { return rtrim(ltrim(s)); }
+
+std::vector<std::string> split(const std::string &input,
+                               const std::string &delimiter) {
+  std::vector<std::string> tokens;
+  auto temp = input;
+  size_t pos = 0;
+  while ((pos = temp.find(delimiter)) != std::string::npos) {
+    std::string token;
+    token = temp.substr(0, pos);
+    string_trimmer::trim(token);
+
+    if (!token.empty()) {
+      tokens.push_back(token);
+    }
+
+    temp.erase(0, pos + delimiter.length());
+  }
+  tokens.push_back(temp);
+
+  return tokens;
+}
+
+} // namespace string_functions
 
 // trim from start (in place)
 
-#endif //ADVENT_OF_CODE_TRIM_FUNCTIONS_H
+#endif // ADVENT_OF_CODE_TRIM_FUNCTIONS_H
--- year2024/day7/part1/cpp/main.cpp
+++ year2024/day7/part1/cpp/main.cpp
@@ -1,7 +1,28 @@
+#include <bitset>
+#include <cmath>
 #include <fstream>
 #include <iostream>
+#include <math.h>
+#include <sstream>
 #include <string>
 #include <trim_functions.h>
+#include <vector>
+
+struct dataRow_t {
+  unsigned long sum;
+  std::vector<unsigned long> numbers;
+};
+
+std::vector<dataRow_t> test_data;
+
+int bits_needed(unsigned int n) { return std::bit_width(n == 0 ? 1u : n); }
+
+unsigned long do_func( unsigned long left, unsigned long right, char operand ) {
+  if(operand == '0') {
+    return left + right;
+  }
+  return left * right;
+}
 
 int main() {
 
@@ -11,19 +32,58 @@ int main() {
     std::cerr << "cannot find file " << input_file << std::endl;
     return 1;
   }
-  long sum = 0;
+  unsigned long sum = 0;
 
   std::string text_line;
 
   while (std::getline(input_data, text_line)) {
     string_trimmer::trim(text_line);
+
     if (text_line.empty()) {
       break;
     }
+    auto pos = text_line.find(':');
+    if (pos == std::string::npos) {
+      std::cerr << "The input file has a wrong format\n";
+      input_data.close();
+    }
+    auto sumstr = text_line.substr(0, pos);
+    auto result = string_functions::split(text_line.substr(pos + 1), " ");
+    dataRow_t data;
+    data.sum = std::stol(sumstr);
+    for (const auto &numberstr : result) {
+      data.numbers.push_back(std::stol(numberstr));
+    }
+
+    test_data.push_back(data);
   }
 
   input_data.close();
 
-  std::cout << "Solution: " << std::endl;
+  for (const auto &data : test_data) {
+    std::cout << "bitset: " << std::bitset<64>(data.numbers.size()) << "\n";
+    std::stringstream ss;
+    auto count_spaces = data.numbers.size()-1;
+    auto combinations_count = static_cast<unsigned int>(pow(2, count_spaces));
+    auto space_needed = bits_needed(combinations_count-1);
+    for (int i = 0; i < combinations_count; ++i) {
+      ss << std::bitset<64>(i);
+      auto operators = ss.str();
+      operators.erase(0, operators.size() - space_needed);
+      std::cout << operators << "\n";
+      auto result = data.numbers[0];
+      for(int n = 1; n < data.numbers.size();  ++n ) {
+         result = do_func( result, data.numbers[n], operators[n-1] );
+      }
+      if (result == data.sum ) {
+        std::cout << "Found: " << result << "\n";
+        sum += data.sum;
+        break;
+      }
+
+    }
+  }
+
+  std::cout << "Solution: " << sum << std::endl;
   return 0;
 }
