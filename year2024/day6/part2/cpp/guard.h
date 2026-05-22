77ccfb9f685072c165ac02d2d7f3ac2572a162e8
Harry Wagner <wagner.harry@siemens.com>
2026-05-22 12:53:15 +0200

    refactored the Direction class

--- year2024/day6/part2/cpp/guard.h
+++ year2024/day6/part2/cpp/guard.h
@@ -8,128 +8,89 @@
 class Position {
 
 public:
-    Position() = default;
-    Position(int x, int y) : _x(x), _y(y) {}
-    void SetX(int x) {_x = x;};
-    void SetY(int y) {_y = y;};
-    [[nodiscard]] int X() const {
-        return _x;
-    }
+  Position() = default;
+  Position(int x, int y) : _x(x), _y(y) {}
+  void SetX(int x) { _x = x; };
+  void SetY(int y) { _y = y; };
+  [[nodiscard]] int X() const { return _x; }
 
-    [[nodiscard]] int Y() const {
-        return _y;
+  [[nodiscard]] int Y() const { return _y; }
+  bool operator==(const Position &other) const {
+    return _x == other._x && _y == other._y;
+  }
+  bool operator<(const Position &other) const {
+    if (_x < other._x) {
+      return true;
     }
-    bool operator==(const Position &other) const {
-        return _x == other._x && _y == other._y;
+    if (_x == other._x && _y < other._y) {
+      return true;
     }
-    bool operator<(const Position &other) const {
-        if( _x < other._x ) {
-            return true;
-        }
-        if( _x == other._x && _y < other._y ) {
-            return true;
-        }
-        return false;
-    }
+    return false;
+  }
+
 private:
-    int _x {0};
-    int _y {0};
+  int _x{0};
+  int _y{0};
 };
 
-class Direction {
+class Direction : public Position {
 public:
-    Direction() = default;
-    Direction(int x, int y) : _x(x), _y(y) {}
-    void SetX(int x) {
-        _x = x;
-    }
+  Direction() = default;
+  Direction(int x, int y) : Position(x, y) {}
 
-    void SetY(int y) {
-        _y = y;
-    }
+  void TurnRight() {
+    const auto direction = GetRight();
+    SetX(direction.X());
+    SetY(direction.Y());
+  }
 
-    [[nodiscard]] int X() const {
-        return _x;
-    }
-
-    [[nodiscard]] int Y() const {
-        return _y;
-    }
-
-    void TurnRight() {
-        const auto direction = GetRight();
-        _x = direction.X();
-        _y = direction.Y();
-    }
-
-    [[nodiscard]] Direction GetRight() const {
-        const Direction new_direction (_y * -1, _x * 1);
-        return new_direction;
-    }
-
-    bool operator==(const Direction &other) const {
-        return _x == other._x && _y == other._y;
-    }
-
-    bool operator<(const Direction &other) const {
-        if( _x < other._x ) {
-            return true;
-        }
-        if( _x == other._x && _y < other._y ) {
-            return true;
-        }
-        return false;
-    }
-private:
-    int _x {0};
-    int _y {-1};
-
+  [[nodiscard]] Direction GetRight() const {
+    const Direction new_direction(Y() * -1, X() * 1);
+    return new_direction;
+  }
 };
 
 class Guard {
 
 public:
+  Guard() = default;
+  Guard(const Position &position, const Direction &direction)
+      : _position(position), _direction(direction) {}
 
-    Guard() = default;
-    Guard(const Position &position, const Direction &direction)
-        : _position(position),
-          _direction(direction) {
+  [[nodiscard]] int GetPositionX() const { return _position.X(); }
+  [[nodiscard]] int GetPositionY() const { return _position.Y(); }
+
+  [[nodiscard]] int GetDirectionX() const { return _direction.X(); }
+  [[nodiscard]] int GetDirectionY() const { return _direction.Y(); }
+  void TurnRight() { _direction.TurnRight(); }
+  [[nodiscard]] Direction GetTurnRightDirection() const {
+    return _direction.GetRight();
+  }
+
+  [[nodiscard]] Direction GetDirection() const { return _direction; }
+  void SetDirection(const Direction &direction) { _direction = direction; }
+
+  [[nodiscard]] Position GetPosition() const;
+  void SetPosition(Position position) { _position = position; }
+  [[nodiscard]] Guard GetNextPosition() const;
+
+  bool operator<(const Guard &other) const {
+    if (_position < other._position) {
+      return true;
     }
-
-    [[nodiscard]] int GetPositionX() const {return _position.X();}
-    [[nodiscard]] int GetPositionY() const {return _position.Y();}
-
-    [[nodiscard]] int GetDirectionX() const {return _direction.X();}
-    [[nodiscard]] int GetDirectionY() const {return _direction.Y();}
-    void TurnRight() {_direction.TurnRight();}
-    [[nodiscard]] Direction GetTurnRightDirection() const {return _direction.GetRight();}
-
-    [[nodiscard]] Direction GetDirection()const {return _direction;}
-    void SetDirection(const Direction& direction) {_direction = direction;}
-
-    [[nodiscard]] Position  GetPosition() const;
-    void SetPosition(Position position){_position = position;}
-    [[nodiscard]] Guard GetNextPosition() const;
-
-    bool operator<(const Guard &other) const {
-        if( _position < other._position ) {
-            return true;
-        }
-        if( _position == other._position ) {
-            return _direction < other._direction;
-        }
-        return false;
+    if (_position == other._position) {
+      return _direction < other._direction;
     }
+    return false;
+  }
 
-    bool operator==(const Guard &other) const {
-        return (_position == other._position && _direction == other._direction);
-    }
-
+  bool operator==(const Guard &other) const {
+    return (_position == other._position && _direction == other._direction);
+  }
 
 private:
-    Position _position;
-    Direction _direction;
-
+  Position _position;
+  Direction _direction;
 };
 
-#endif //ADVENT_OF_CODE_GUARD_H
+#endif // ADVENT_OF_CODE_GUARD_H
